3a6a2202701103d18c34e148e5e5e37ac863bb64
FabioBabaglioni <fabiobabaglioni@gmail.com>
2023-01-24 16:03:20 +0100

    Istanzio due oggetti movie e stampo a schermo

--- index.php
+++ index.php
@@ -14,11 +14,15 @@
         }
 
         public function getAllMovie(){
-            return "Titolo: " . $this -> title . "<br>" . " Genere: " . $this -> genre . "<br>" . " Regista: " . $this -> director;
+            return "Titolo: " . $this -> title . "<br>" . " Genere: " . $this -> genre . "<br>" . " Regista: " . $this -> director . "<br><br>";
         }
     }
 
     $movie1 = new Movie("Cado dalle nubi", "Commedia", "Checco Zalone");
+    $movie2 = new Movie("Tre uomini e una gamba", "Commedia", "Aldo, Giovanni e Giacomo");
+
 
     echo $movie1 -> getAllMovie();
+    echo $movie2 -> getAllMovie();
+
 ?>
