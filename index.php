a358639bd2e50c0e8d6d0d51cc7fa664315b860e
FabioBabaglioni <fabiobabaglioni@gmail.com>
2023-01-24 16:15:08 +0100

    Re factoring con array

--- index.php
+++ index.php
@@ -18,11 +18,12 @@
         }
     }
 
-    $movie1 = new Movie("Cado dalle nubi", "Commedia", "Checco Zalone");
-    $movie2 = new Movie("Tre uomini e una gamba", "Commedia", "Aldo, Giovanni e Giacomo");
+    $movies= [new movie("Cado dalle nubi", "Commedia", "Checco Zalone"), 
+            new movie("Tre uomini e una gamba", "Commedia", "Aldo, Giovanni e Giacomo")
+        ];
 
-
-    echo $movie1 -> getAllMovie();
-    echo $movie2 -> getAllMovie();
-
+    foreach($movies as $movie) {
+        echo $movie->getAllMovie();
+    }
+    
 ?>
