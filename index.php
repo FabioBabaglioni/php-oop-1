35c34498ed271c38c384c966b032eb0be70d734e
FabioBabaglioni <fabiobabaglioni@gmail.com>
2023-01-24 16:19:03 +0100

    esercizio base completo

--- index.php
+++ index.php
@@ -13,17 +13,22 @@
             $this->director = $director;
         }
 
+        // funzione per ritornare i dati da stampare in pagina 
         public function getAllMovie(){
             return "Titolo: " . $this -> title . "<br>" . " Genere: " . $this -> genre . "<br>" . " Regista: " . $this -> director . "<br><br>";
         }
     }
 
-    $movies= [new movie("Cado dalle nubi", "Commedia", "Checco Zalone"), 
-            new movie("Tre uomini e una gamba", "Commedia", "Aldo, Giovanni e Giacomo")
+    // creo un array per istanziare nuovi film 
+    $movies= [
+            new movie("Cado dalle nubi", "Commedia", "Checco Zalone"), 
+            new movie("Tre uomini e una gamba", "Commedia", "Aldo, Giovanni e Giacomo"),
+            new movie("Titanic", "Romantico", "James Cameron")
         ];
 
+    // creo il ciclo che mi va a tampare a video i dati richiamando la funzione 
     foreach($movies as $movie) {
-        echo $movie->getAllMovie();
+        echo $movie -> getAllMovie();
     }
     
 ?>
